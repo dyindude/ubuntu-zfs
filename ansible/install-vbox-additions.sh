0d3a877e399d15a9db42e8262bf6b74e8335fe90
dyindude <dyindude@github.dyindude.net>
2019-10-06 11:40:59 -0500

    #7 additions still not working - but I've a feeling the .run package doesn't honor the chroot environment and is just checking uname for kernel version - so it'll probably be necessary to do a 2nd stage build of packer just to get guest additions going

--- ansible/install-vbox-additions.sh
+++ ansible/install-vbox-additions.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-mkdir /tmp/vbox
-mount -t iso9660 -o loop /root/VBoxGuestAdditions.iso /tmp/vbox
-/tmp/vbox/VBoxLinuxAdditions.run --nox11
-umount /tmp/vbox
-rm -rf vbox /root/VBoxGuestAdditions.iso
-sync
+mkdir /tmp/vbox && mount -t iso9660 -o loop /root/VBoxGuestAdditions.iso /tmp/vbox && /tmp/vbox/VBoxLinuxAdditions.run --nox11 && umount /tmp/vbox && rm -rf vbox /root/VBoxGuestAdditions.iso && sync
